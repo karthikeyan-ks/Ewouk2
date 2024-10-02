aa4d167f821b0ecc32cde9a8335a7fbf6bf80f71
karthikeyan-ks <karthikeyanks3673@gmail.com>
2024-10-02 09:36:16 +0530

    pages and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Loading } from './component/Loading'
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isVisible, setIsVisible] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 3000); // Removes the element after 3 seconds
+
+    return () => clearTimeout(timer); // Cleanup the timer when component unmounts
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      {
+        isVisible && (
+          <Loading/>
+        )
+      }
+    </div>
   )
 }
 
---
+++ src/component/Loading.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import './Loading.css'
+export const Loading = () => {
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((prevProgress) => {
+                if (prevProgress >= 100) {
+                    clearInterval(timer); 
+                    return 100;
+                }
+                return prevProgress + 1; 
+            });
+        }, 20); // Adjust the interval for a smooth transition over 3 seconds
+
+        return () => clearInterval(timer); // Cleanup the interval on unmount
+    }, []);
+    return (
+        <div className='loading'>
+            <div style={{ width: '60%', backgroundColor: '#e0e0df', borderRadius: '10px' }}>
+                <div
+                    style={{
+                        width: `${progress}%`,
+                        backgroundColor: '#76c7c0',
+                        height: '10px',
+                        borderRadius: '10px',
+                        transition: 'width 0.3s ease-in-out',
+                    }}
+                >
+                    <span style={{ marginLeft: '5px', color: '#fff' }}>{progress}%</span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
