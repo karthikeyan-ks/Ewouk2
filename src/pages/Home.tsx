231204671556f03f465767d926f77bfbdc1d1ed1
SUFIYANJT <jtfinu@gmail.com>
2024-10-02 13:24:50 +0530

    first page center

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,18 @@
 import './Home.css'
 import video from '../assets/video/Video-24.mp4'
+import logo from '../static/image/image.png';
+
 export const Home = () => {
     return (
         <div className='home-container'>
             <video src={video} className="video" autoPlay muted loop></video>
             <div className="gradient-overlay"></div>
-            <div className='bigText'>
-                <h1>
-                    Ewouk   
-                </h1>
+
+            <div className="ewouk-container">
+                <div className='brand-logo'> 
+                    <img src={logo} alt="E-WOUK logo" />
+                </div>
+                <h1 className="ewouk-text">E-WOUK</h1>
             </div>
         </div>
     )
